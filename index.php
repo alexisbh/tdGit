c7ca2d18f3a456ac3f45e2ca4b97c67a18ee0e50
Dev <dev@lyc-lecastel.fr>
2015-01-14 09:30:10 +0100

    Nouvelle organisation et utilisation de php
    Signed-off-by: Dev <dev@lyc-lecastel.fr>

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "..//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<meta name="author" content="SIO2" />
+		<meta name="keywords" content="Mon Appli Web" />
+		<meta name="description" content="Appli Web pour le TP Git" />
+		<meta name="date" content="2012-12-22T08:30:45+01:00" />
+		<!-- <link rel="shortcut icon" type="image/x-icon" href="monIcon.ico" /> -->
+		<link rel="stylesheet" href="monCss.css">
+		<title>Mon application Web</title>
+		<script type="text/javascript" src="monScript.js"></script>
+	</head>
+	<?php include 'header.html'; ?>
+	<div id="contenuPage"><?php include 'calculatrice.html'; ?> </div>
+	<?php include 'footer.html'; ?>
+</html>
